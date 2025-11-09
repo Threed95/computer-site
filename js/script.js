27806cc463b3fe7f47d489d4250a6f909d596a03
Threed95 <bolshanovad95@gmail.com>
2025-11-09 15:53:01 +0300

    добавила js в div program-line__content

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const contents = document.querySelectorAll('.program-line__content');
+
+contents.forEach(item => {
+    const titles = item.querySelector('.program-line__title');
+    const descr = item.querySelector('.program-line__descr');
+
+    titles.addEventListener('click', () => {
+
+        contents.forEach(el => {
+            const currentDescr = el.querySelector('.program-line__descr');
+            currentDescr.classList.remove('active');
+        });
+
+
+        descr.classList.add('active');
+    });
+});
